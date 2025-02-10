d3624ea6b0ed6644fc37eef9d8938afba4cc2207
Facundo Cubells <facundocubells@gmail.com>
2025-02-10 09:23:46 -0300

    l

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ import Testimonios from "./components/Testimonios";
 import Footer from "./components/Footer";
 import  Links  from "./components/Links";
 
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App text-gray-800 background-main">
-        
+      <head>
+        <title>Escuela de Carpinteros de Laboratorio</title> 
+        <meta name="description" content="Aprende carpintería de laboratorio con nuestro curso online. ¡Inscríbete ahora!" />
+        <link rel="icon" href="/favicon.ico" /> 
+        <meta property="og:title" content="Carpintería de Laboratorio - ¡Inscríbete ahora!" />
+        <meta property="og:description" content="Domina el arte de la madera y crea tus propios proyectos." />
+        <meta property="og:image" content="/banner.jpg" /> 
+        <meta property="og:url" content="https://tu-sitio-web.com/" /> 
+      </head>
 
         <Routes>
           <Route
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
+    
     <footer className="bg-verde-osc text-white py-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
